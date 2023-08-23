5dfa57c479456cfdac7a4d9c03264207861bba9e
ImMyeongJang <lkut1@naver.com>
2023-08-23 15:18:18 +0900

    커밋1

---
+++ 퀴즈.py
@@ -0,0 +1,9 @@
+Python 3.11.4 (tags/v3.11.4:d2340ef, Jun  7 2023, 05:45:37) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+>>> num1=10
+>>> num2=20
+>>> print(num1, num2)
+10 20
+>>> print(num2, num1)
+20 10
